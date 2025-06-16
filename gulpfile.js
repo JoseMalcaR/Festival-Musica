22a9d50c891875f9156c4285683d407b7095b509
JoseMalcaR <jmalca@utem.cl>
2025-06-16 15:53:57 -0400

    installing sass and compiling sass whit gulp(enable watch mode).

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+import * as dartSass from 'sass';
+import gulpSass from 'gulp-sass';
+import { src, dest, watch } from 'gulp';
+
+
+const sass = gulpSass(dartSass);
+
+export function css(done) {
+    src('src/scss/app.scss')
+        .pipe( sass())
+        .pipe( dest('dist/css') )
+
+    done();
+}
+
+
+export function dev() {
+    watch('src/scss/app.scss', css);
+    
+}
