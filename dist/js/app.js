747df92f898545d1bcca7bb4020a95764484a621
JoseMalcaR <jmalca@utem.cl>
2025-06-24 16:53:59 -0400

    add gallery with js and create modal

---
+++ dist/js/app.js
@@ -0,0 +1,63 @@
+document.addEventListener('DOMContentLoaded', () => {
+    crearGaleria();
+});
+
+function crearGaleria() {
+    const galeria = document.querySelector('.galeria-imagenes');
+    const cantidad_imagenes = 16; // Total de imágenes en la galería
+
+
+    for(let i = 1; i <=cantidad_imagenes; i++) {
+        const imagen = document.createElement('IMG')
+        imagen.src = `src/img/gallery/full/${i}.jpg`;
+        imagen.alt = 'Imagen Galeria';
+
+        // Event Handler
+        imagen.onclick = function() {
+            mostrarImagen(i);
+        }
+       
+        galeria.appendChild(imagen);
+
+    }
+}
+
+function mostrarImagen(i){
+    const imagen = document.createElement('IMG')
+    imagen.src = `src/img/gallery/full/${i}.jpg`;
+    imagen.alt = 'Imagen Galeria';
+
+    
+    // Generar Modal
+    const modal = document.createElement('DIV');
+    modal.classList.add('modal');
+    modal.onclick = cerrarModal;
+    
+
+    // Botón de cerrar
+    const cerrarModalBtn = document.createElement('BUTTON');
+    cerrarModalBtn.textContent = 'X';
+    cerrarModalBtn.classList.add('btn-cerrar');
+    cerrarModalBtn.onclick = cerrarModal; // Cerrar modal al hacer click
+
+    modal.appendChild(cerrarModalBtn);
+    modal.appendChild(imagen);
+
+    // Agregar al html
+    const body = document.querySelector('body');
+    body.classList.add('overflow-hidden'); // Evitar scroll
+    body.appendChild(modal);
+
+}
+
+function cerrarModal() {
+    const modal = document.querySelector('.modal');
+    modal.classList.add('fade-out');
+
+    setTimeout(() => {
+    modal?.remove()
+
+    const body = document.querySelector('body');
+    body.classList.remove('overflow-hidden');
+    }, 500);
+}
